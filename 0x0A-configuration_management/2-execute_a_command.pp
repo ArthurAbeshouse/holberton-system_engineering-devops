e698b3fc60615374cb7a2484db615e7259c028b0
ArthurAbeshouse <artieabe99@gmail.com>
2020-03-23 22:39:37 +0000

    fixed spacing issues

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # Creates a manifest that kills a process named killmenow
 exec { 'killmenow':
-     path     => '/usr/bin',
-     command  => 'pkill -f killmenow',
-     provider => 'shell'
+  path    => '/usr/bin',
+  command => 'pkill -f killmenow'
 }
