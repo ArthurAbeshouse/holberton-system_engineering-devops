3809894cf92074d53d9cc94544f3f75c9d4b53f8
ArthurAbeshouse <artieabe99@gmail.com>
2020-05-04 22:10:17 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,20 +11,15 @@ tasks_url = "https://jsonplaceholder.typicode.com/todos/"
 
 def data():
     """Parses data form API"""
-    if not len(argv):
-        return print("Invalid")
     empo_id = argv[1]
-    empo = get(empo_url + empo_id).json()["username"]
+    empo = get(empo_url + empo_id).json()
     tasks = get(tasks_url).json()
     with open(empo_id + ".csv", "w") as file:
-        Task_writer = writer(
-            file,
-            delimiter=",",
-            quotechar='"',
-            quoting=QUOTE_ALL)
+        Task_writer = writer(file, delimiter=",", quotechar='"',
+                             quoting=QUOTE_ALL)
         for i in tasks:
             Task_writer.writerow(
-                [empo_id, empo, i["completed"], i["title"]])
+                [empo_id, empo["username"], i["completed"], i["title"]])
 
 
 if __name__ == "__main__":
