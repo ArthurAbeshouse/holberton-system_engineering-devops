30dd5396ea6131ae5b627e9ad895f416b1be4d45
ArthurAbeshouse <artieabe99@gmail.com>
2020-05-06 17:28:12 +0000

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,22 +2,24 @@
 """Exports data to json"""
 from json import dump
 from requests import get
+
 empo_url = "https://jsonplaceholder.typicode.com/users/"
-tasks_url = "https://jsonplaceholder.typicode.com/todos/"
+tasks_url = "https://jsonplaceholder.typicode.com/todos"
 
 
 def data():
     """Parses data form API"""
     empo = get(empo_url).json()
-    task_list = []
     dict_new = {}
     for u in empo:
         empo_id = str(u.get("id"))
         tasks = get(tasks_url + "?userId=" + empo_id).json()
+        task_list = []
         for i in tasks:
-            task_list.append({"username": u.get("username"),
-                              "task": i.get("title"),
-                              "completed": i.get("completed")})
+            if i["userId"] != empo_id:
+                task_list.append({"username": u["username"],
+                                  "task": i["title"],
+                                  "completed": i["completed"]})
         dict_new[empo_id] = task_list
     with open("todo_all_employees.json", "w") as file:
         dump(dict_new, file)
