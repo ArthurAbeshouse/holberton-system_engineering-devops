40d795f74daee0008f5b7ecc676699da7ccec590
ArthurAbeshouse <artieabe99@gmail.com>
2020-05-06 14:39:34 +0000

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,17 +11,24 @@ def data():
     empo = get(empo_url).json()
     task_list = []
     dict_new = {}
+    user_dict = {}
+    user_name = {}
     for u in empo:
-        empo_id = str(u["id"])
-        tasks = get(tasks_url + "?userId=" + empo_id).json()
-        for i in tasks:
-            task_list.append({"username": u["username"],
-                              "task": i["title"],
-                              "completed": i["completed"]})
-            dict_new[empo_id] = task_list
+        empo_id = u.get("id")
+        user_dict[empo_id] = []
+        user_name[empo_id] = u.get("username")
+#        tasks = get(tasks_url,"{}").foarmat(empo_id).json()
+#        tasks = get(tasks_url + "?userId=" + empo_id).json()
+    for i in get(tasks_url).json():
+        uid = i.get("userId")
+        task_list.append({"username": user_name.get(uid),
+                          "task": i["title"],
+                          "completed": i["completed"]})
+        dict_new[uid] = task_list
     with open("todo_all_employees.json", "w") as file:
         dump(dict_new, file)
 
 
 if __name__ == "__main__":
     data()
+#userdict.get(uid).append(taskdict)
