9e2b193f5af1d81c8ee9a70be2703bc48389b1d3
ArthurAbeshouse <artieabe99@gmail.com>
2020-05-07 19:54:01 +0000

    Recurse it!

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Returns a list containing the titles
+   of all the hottest articles for a given subreddit"""
+from requests import get
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """Returns a list containing the titles
+       of all the hottest articles for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {"User-agent": "agent_Flemming"}
+    response = get(url, headers=headers, allow_redirects=False)
+    if (response.status_code != 200):
+        return None
+    else:
+        jsonresponse = response.json()
+        data = jsonresponse["data"]["after"]
+        parent = jsonresponse["data"]["children"]
+        for post in parent:
+            hot_list.append(post["data"]["title"])
+        if data is None:
+            return hot_list
+        return recurse(subreddit, hot_list, data)
